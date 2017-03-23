02bcda8f5457c0475fb40c2d33c0530957adf503
Jorge <jcarapia83@Gmail.com>
2017-03-23 04:12:39 -0700

    slideshow works, will refactor ot grap info from external file

--- src/SlideShow.js
+++ src/SlideShow.js
@@ -1,12 +1,55 @@
 import React, { Component } from 'react'
 
 class SlideShow extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      image: "../public/images/park-img/arches.jpg",
+      caption: 'Arches National Park'
+    }
+  }
+
+  componentDidMount() {
+    setInterval(()=>this.imageSetter(), 5000);
+  }
+
+  imageSetter() {
+    console.log('hello', this.state.caption)
+    let parkImages = [
+      {
+        image: "../public/images/park-img/arches.jpg",
+        caption: 'Arches National Park'
+      },
+      {
+        image: "../public/images/park-img/crater.jpg",
+        caption: 'Crater Lake National Park'
+      },
+      {
+        image: "../public/images/park-img/grandcanyon.jpg",
+        caption: 'Grand Canyon National Park'
+      },
+      {
+        image: "../public/images/park-img/joshua.jpg",
+        caption: 'Joshua Tree National Park'
+      },
+    ];
+
+    let randomIndex = Math.floor(Math.random() * parkImages.length);
+    console.log(randomIndex)
+    let newImage = parkImages[randomIndex].image;
+    let newCaption = parkImages[randomIndex].caption;
+    
+    this.setState({image: newImage, caption: newCaption})
+  }
+
   render() {
+
     return (
       <figure className="slideshow">
-        <img src="../public/images/park-img/arches.jpg" alt="arches"/>
+        <img src={this.state.image} alt={this.state.caption}/>
         <figcaption>
-          Arches National Park
+          {this.state.caption}
         </figcaption>
       </figure>
     )
