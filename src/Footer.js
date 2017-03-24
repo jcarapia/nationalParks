8da86283262af2a48035ffe9f6ea624235446f97
Jorge <jcarapia83@Gmail.com>
2017-03-23 21:58:17 -0700

    added park codes

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
     return (
       <footer>
         <img className="logo-small" src="../public/images/logo-title-right.png" alt="logo"/>
-        <p>Created by Jorge Carapia</p>
+        <p></p>
         <ul className="social-media">
           <li><a href="https://www.facebook.com/RichmondGeorge" target="_blank"><i className="ion-social-facebook"></i></a></li>
           <li><a href="https://www.linkedin.com/in/jcarapia/" target="_blank"><i className="ion-social-linkedin"></i></a></li>
